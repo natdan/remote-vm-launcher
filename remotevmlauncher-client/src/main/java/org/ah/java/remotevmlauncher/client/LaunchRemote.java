144b3a523a06d9889c09ec4be4ffe58a8446a9f2
Daniel Sendula <natdan@users.noreply.github.com>
2019-10-01 15:47:14 +0100

    Simple update

--- remotevmlauncher-client/src/main/java/org/ah/java/remotevmlauncher/client/LaunchRemote.java
+++ remotevmlauncher-client/src/main/java/org/ah/java/remotevmlauncher/client/LaunchRemote.java
@@ -1,15 +1,15 @@
 /*
  * Copyright (c) 2013 Creative Sphere Limited.
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * Contributors:
  *
@@ -93,7 +93,7 @@ public class LaunchRemote {
     private MainClassProcessor mainClassProcessor = new MainClassProcessor();
     private StartApplicationProcessor startApplicationProcessor = new StartApplicationProcessor();
     private StartRemoteVMProcessor startRemoteVMProcessor = new StartRemoteVMProcessor();
-    
+
     private Set<String> excludeClassPath = new LinkedHashSet<String>();
 
     public LaunchRemote() {
@@ -247,7 +247,7 @@ public class LaunchRemote {
                 if (!filePath.equals(launchersPath)) {
                     String[] filePathComponents = filePath.split("/");
                     String relativisedPath = PathUtils.relativisePath(herePathComponents, filePathComponents);
-                    
+
                     if (!excludeClassPath.contains(relativisedPath) && !excludeClassPath.contains(filePath)) {
                         String pathId = localClasspathProcessor.getLocalClasspath().get(relativisedPath);
                         if (pathId == null) {
